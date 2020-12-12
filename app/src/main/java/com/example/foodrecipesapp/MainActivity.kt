ec8389642b373cf00411ce47fec959f3498e8485
william <williamlopes64@gmail.com>
2020-12-12 16:14:07 -0300

    Navigation component setup: Implementing bottom navigation menu into the project, in order to display: Recipes, Favorites and Joke. For that I should create three fragments, bottom_nav_menu.xml and set them up in MainActivity.kt, that's all.

--- app/src/main/java/com/example/foodrecipesapp/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipesapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.foodrecipesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.recipesFragment, R.id.favoriteRecipesFragment, R.id.foodJokeFragment))
+
+        bottom_navigation_view.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp() || super.onSupportNavigateUp()
 }
