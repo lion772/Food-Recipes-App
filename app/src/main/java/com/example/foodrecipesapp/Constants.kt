1190edd449ac4b53026cd1bbd33123f627224b11
william <williamlopes64@gmail.com>
2020-12-16 19:16:00 -0300

    Creating Constants.kt and arranging MainActivity.kt and Fragments into UI package.

---
+++ app/src/main/java/com/example/foodrecipesapp/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.foodrecipesapp
+
+class Constants {
+
+    companion object {
+
+        const val API_KEY = "74cefc4fdba2403c99b77d3d21b89c48"
+        const val BASE_URL = "https://api.spoonacular.com"
+    }
+}
--- app/src/main/java/com/example/foodrecipesapp/ui/fragment/foodjoke/FoodJokeFragment.kt
+++ app/src/main/java/com/example/foodrecipesapp/ui/fragment/foodjoke/FoodJokeFragment.kt
@@ -1,25 +1,21 @@
-package com.example.foodrecipesapp
+package com.example.foodrecipesapp.ui.fragment.foodjoke
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_recipes.view.*
+import com.example.foodrecipesapp.R
 
 
-class RecipesFragment : Fragment() {
+class FoodJokeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
-
-        view.recycler_view.showShimmer()
-
-        return view
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_food_joke, container, false)
     }
-
 
 }
