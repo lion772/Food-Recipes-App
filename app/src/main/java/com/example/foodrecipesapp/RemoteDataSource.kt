bda4a476ab7b6642add29e237a2441076720bab5
william <williamlopes64@gmail.com>
2020-12-20 11:26:29 -0300

    Injecting FoodRecipesApi.kt in RemoteDataSource.kt

--- app/src/main/java/com/example/foodrecipesapp/RemoteDataSource.kt
+++ app/src/main/java/com/example/foodrecipesapp/RemoteDataSource.kt
@@ -1,6 +1,11 @@
 package com.example.foodrecipesapp
 
-class RemoteDataSource {
+import retrofit2.http.QueryMap
+import javax.inject.Inject
 
+class RemoteDataSource @Inject constructor(
+        private val foodRecipesApi: FoodRecipesApi
+) {
+    suspend fun getRecipes(queries: Map<String, String>) = foodRecipesApi.getRecipes(queries)
 
 }
